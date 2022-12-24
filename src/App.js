d15e411bc45c9addd548b18f604787478d299d06
Marcus <vidranyimarcus@gmail.com>
2022-12-24 01:51:44 +0100

    another day

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import { useState, useRef } from "react";
 import ScrollToTop from './components/ScrollToTop';
 
@@ -10,9 +10,10 @@ function App() {
   const aboutMe = useRef(null);
   const myProjects = useRef(null);
 
-  const scrollToSection = (elementRef) => {
+  const scrollToSection = (ref) => {
     window.scrollTo({
-      top: elementRef.current.offsetTop,
+      top: ref.current.offsetTop,
+      left: 0,
       behavior: "smooth",
     });
   };
@@ -32,12 +33,12 @@ function App() {
     <div className="App">
       <ScrollToTop />
       <nav className="nav">
-        <a href="#" className="marcus_email">marcusvidranyi@gmail.com</a>
+        <a href="email" className="marcus_email">marcusvidranyi@gmail.com</a>
         <ul className={active}>
-          <li onClick={() => scrollToSection(home)} className="nav_item"><a href="#" className="nav_link">Home</a></li>
-          <li onClick={() => scrollToSection(services)} className="nav_item"><a href="#" className="nav_link">My Services</a></li>
-          <li onClick={() => scrollToSection(aboutMe)} className="nav_item"><a href="#" className="nav_link">About me</a></li>
-          <li onClick={() => scrollToSection(myProjects)} className="nav_item"><a href="#" className="nav_link">My Projects</a></li>
+          <li onClick={() => scrollToSection(home)} className="nav_item"><a href="#home" className="nav_link">Home</a></li>
+          <li onClick={() => scrollToSection(services)} className="nav_item"><a href="#services" className="nav_link">My Services</a></li>
+          <li onClick={() => scrollToSection(aboutMe)} className="nav_item"><a href="#aboutme" className="nav_link">About me</a></li>
+          <li onClick={() => scrollToSection(myProjects)} className="nav_item"><a href="#myprojects" className="nav_link">My Projects</a></li>
         </ul>
         <div onClick={navToggle} className={toggleIcon}>
           <div className="line1"></div>
@@ -46,18 +47,54 @@ function App() {
         </div>
       </nav>
       <main>
-        <div ref={home} className='home_section'>
-          <h3>Home</h3>
+
+        {/* Intro */}
+        <section ref={home} className='intro'>
+          <h1 className="section_title section_title_intro" >
+            Hi, I am <strong>Marcus Vidrányi</strong>
+          </h1>
+          <p className="section_subtitle section_subtitle_intro">Front-End Developer</p>
+          <img src="https://picsum.photos/200/300" alt="my picture" className="intro_img"></img>
+        </section>
+
+        {/* Services */}
+
+        <div ref={services} className='my_services'>
+          <h2 className="section_title section_title_services">What I do</h2>
+          <div className="services">
+            <div className="service">
+              <h3>Design</h3>
+              <p>I strive to create beautiful and intuitive web apps, meaning I design with both aestetics and usability in mind. You will get your designs delivered as Figma files, as websites or both.</p>
+            </div>
+            <div className="service">
+              <h3>Development</h3>
+              <p>I am a versatile frontend developer with a speciality in React JS and Tailwind CSS. I also have a solid grip of the fundamentals of web development using HTML, CSS, and JavaScript.</p>
+            </div>
+          </div>
+
+          <a href="#myprojects" className="btn">My Projects</a>
         </div>
-        <div ref={services} className='my_services_section'>
-          <h3>My Services</h3>
-        </div>
-        <div ref={aboutMe} className='about_me_section'>
-          <h3>About me</h3>
-        </div>
+
+
+
+      
+
+        <section ref={aboutMe} className='about_me'>
+        <h2 className="section_title section_title_about">Who I am</h2>
+           <p className="section_subtitle section_subtitle_about">Front-End Developer from Budapest</p>
+           
+           <div className="about_me_body">
+               <p>I'm a web developer and designer based out of Budapest, Hungary. I love building apps that solve real-world problems, and that are delightful to use. My specialities include Javascript, React JS, Tailwind CSS, and Styled Components.</p>
+               <p>My background is in teaching and marketing, and I have a bachelors degree in English from Kings College. I also have five adorable cats.</p>
+           </div>
+           
+           <img src="https://picsum.photos/200/300" alt="Jane leaning against a bus" className="about_me_img" />
+        </section>
+
         <div ref={myProjects} className='my_projects_section'>
           <h3>My Projects</h3>
         </div>
+
       </main>
     </div>
   );
